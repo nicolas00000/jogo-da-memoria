a84b819f2e90bcfc52b9f3efc0bd922aaad44723
Nicolas Mariano <nicmariano10@gmail.com>
2022-04-29 20:12:58 -0300

    adicionar cartas por js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const imagens = [
     "unicornparrot",
     "bobrossparrot"   ,
@@ -7,7 +8,42 @@ const imagens = [
     "revertiparrot",
     "tripleparrot"
 ]
+imagens.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
 
-for(var i = 0; i < imagens.length; i++){
-    console.log(i)
+
+// Esta função pode ficar separada do código acima, onde você preferir
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+
+let qtdcartas = parseInt(prompt("quantidades de cartas para jogo"))
+while(qtdcartas < 4 || qtdcartas > 16 || (qtdcartas % 2 ) === 1){
+    qtdcartas =  parseInt(prompt("quantidades de cartas para jogo"))
+}
+
+const cartas = []
+for(let i=0; i < qtdcartas/2; i++){
+    const papagaio = imagens[i]
+    cartas.push(papagaio);
+    cartas.push(papagaio);
+}
+
+
+
+const tabuleiro = document.querySelector(".tabuleiro");
+const tiposCartas = ["um", "dois", "tres", "quatro", "cinco", "seis"];
+
+
+
+for(let i = 0; i < b; i++) {
+  tabuleiro.innerHTML += `
+            <li class="carta">
+                <div class='back-face face'>
+                    <img src='imagens/${tiposCartas[i]}.gif'> 
+                </div>
+
+        </li>
+`
+
 }

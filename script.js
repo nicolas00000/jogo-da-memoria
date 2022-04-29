abba9d93c1331a7e1a6493c427d42432d841ab76
Nicolas Mariano <nicmariano10@gmail.com>
2022-04-29 16:59:47 -0300

    front end parrot html css

---
+++ script.js
@@ -0,0 +1,13 @@
+const imagens = [
+    "unicornparrot",
+    "bobrossparrot"   ,
+    "explody.parrot",
+    "fiestaparrot",
+    "metalparrot",
+    "revertiparrot",
+    "tripleparrot"
+]
+
+for(var i = 0; i < imagens.length; i++){
+    console.log(i)
+}
